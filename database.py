5bc03219755c09072e772ea57f8c475baaf047a2
madicee <mahdisaraam@gmail.com>
2024-08-25 15:15:36 +0000

    dynamic pages for each job

--- database.py
+++ database.py
@@ -5,11 +5,21 @@ db_connection_string = os.environ['DB_CONNECTION_STRING']
 
 engine = create_engine(db_connection_string)
 
-def load_data_from_db():
+def load_jobs_from_db():
   with engine.connect() as conn:
     result = conn.execute(text("select * from jobs"))
     jobs = result.all()
     return jobs
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs where id = :val"), {"val": id})
+    rows = result.all()
+    if len(rows) == 0:
+      return None
+    else:
+      return rows[0]
+    
 
 
   
