8272f6e74cdee4d4719f284bf31b6bc11649fb01
madicee <mahdisaraam@gmail.com>
2024-09-02 05:27:27 +0000

    changed it a bit, but data still doesn't get inserted into sql table from back-end

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def show_job(id):
 def apply_to_job(id):
   data = request.form
   job = load_job_from_db(id)
-  add_application_to_db(id, data)
+  add_application_to_db(data)
   return render_template('application_submitted.html', application=data, job=job)  
 
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -17,10 +17,14 @@ def load_job_from_db(id):
     else:
       return rows[0]
 
-def add_application_to_db(job_id, data):
+def load_jobs_from_application():
   with engine.connect() as conn:
-    query = text("insert into application job_id, full_name, email, linkedin_url, education, work_experience, resume_url) values (:jobs{{'job_id'}}, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)")
+    result = conn.execute(text("select * from application"))
+    application = result.all()
+    return application
 
-    conn.execute(query, {"job_id" : job_id, "full_name": data["full_name"], "email": data["email"], "linkedin_url": data["linkedin_url"], "education": data["education"], "work_experience": data["work_experience"], "resume_url": data["resume_url"]})
-  
-  
+def add_application_to_db(application):
+  with engine.connect() as conn:
+    query = text("insert into application (full_name, email, linkedin_url, education, work_experience, resume_url) values (:full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)")
+
+    conn.execute(query, {"full_name": application["full_name"], "email": application["email"], "linkedin_url": application["linkedin_url"], "education": application["education"], "work_experience": application["work_experience"], "resume_url": application["resume_url"]})
